1ed63e68283d5272a5739da19f044f4faaae8c56
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-15 14:05:59 -0700

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints exactly
+ * "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * followed by a new line, to the standard error.
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
