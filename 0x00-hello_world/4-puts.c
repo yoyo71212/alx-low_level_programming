6a31d5d2f1e28e514d6465b216d7e8b47f1d4be8
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-15 11:47:52 -0700

    Fix 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints "\"Programming is like building a multilingual puzzle"'
+ * @parameter: void
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0)
 }
