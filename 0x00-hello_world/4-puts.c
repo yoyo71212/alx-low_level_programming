936c4128b224dcdf83b36370013c42bd9839a827
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-15 11:55:02 -0700

    Fix 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
  * main - Entry point
  *
  * Description: Prints \"Programming is like building a multilingual puzzle
+ * @parameter: void
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
