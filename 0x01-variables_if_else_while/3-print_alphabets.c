3f35ab5aa6bce07c4c2100581e11876f746eb36e
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-16 08:05:04 -0700

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (int = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
