ac705ddaeb0ffc00480f98f9b27056347600b7b5
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-16 10:18:07 -0700

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(48 + i);
+		}
+		else
+		{
+			putchar('A' + (i - 10));
+		}
+	}
+	putchar('\n');
+	return (0);
+}
