07636331573aaa1a43cf56cfa3eb71aaec9ba9b6
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-21 13:38:27 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		int j;
+
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(i / 10);
+			}
+			_putchar(i % 10);
+			_putchar(':');
+			if (j < 10)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(j / 10);
+			}
+			_putchar(j % 10);
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table: Prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 0; j < 10; j++)
+		{
+			if (j < 9)
+			{
+				
+			}
+		}
+	}
+}
