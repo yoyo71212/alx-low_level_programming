23f043c8d3709980671823a1e8b63fef597936db
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-21 12:58:06 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Returns the last digit in a number
+ *
+ * @n: The number
+ *
+ * Return: Returns the last digit in n
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
