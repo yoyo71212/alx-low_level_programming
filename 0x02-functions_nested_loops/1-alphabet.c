50861f42ae8febbe5caf774c0fcba4a0a2466378
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-21 11:54:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
