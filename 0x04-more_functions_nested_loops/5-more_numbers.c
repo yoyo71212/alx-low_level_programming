555654fdf275e6310a5a0031446e8d9b28fe82f4
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-22 11:43:36 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,10 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar('1');
+				_putchar((j / 10) + 48);
 			}
 			_putchar((j % 10) + 48);
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
