459ecb1040f7a4ef54e992a47318cfb517edb60c
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-22 11:53:05 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - Prints a square with an area = size * size
+ *
+ * @size: The length/width of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+
+	if (size < 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{	int j;
+
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
