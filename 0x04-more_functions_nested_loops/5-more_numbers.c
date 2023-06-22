73e17681d347ed2db28cd2c8811114464eee2b47
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-22 02:12:14 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - Prints the numbers 0 to 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n')
+	}
+}
