b73d17c3a589de9908aeab13651252b8f35374f2
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-22 15:31:55 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * isPrime - Given a number it returns 1 if it is prime and 0 otherwise
+ *
+ * @num: The number to be checked
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int isPrime(int num)
+{
+	if (num <= 1)
+	{
+		return (0);
+	}
+
+	int i;
+
+	for (i = 2; i * i < num; i++)
+	{
+		if (num % i == 0 && i != num)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num, i, max;
+
+	num = 612852475143;
+
+	for (i = 2; i * i < num; i++)
+	{
+		if (num % i == 0 && isPrime(i))
+		{
+			max = i;
+		}
+	}
+
+	printf("%d", max);
+	return (0);
+}
