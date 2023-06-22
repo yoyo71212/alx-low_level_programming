6f5b10c5efd86d506d6fed0d3115c73bfee0cf6d
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-21 18:43:23 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,34 +1,55 @@
 #include "main.h"
 /**
+ * print_to_98 - Prints all numbers from n to 98
+ *
+ * @n: The initial number
+ *
+ * Return: void
  */
 void print_to_98(int n)
 {
 	int i;
-	
+
 	if (n <= 98)
 	{
 		for (i = n; i < 99; i++)
 		{
-			_putchar(i + '0');
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
+			}
 			if (i < 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
-		_putchar('\n');
 	}
 	else
 	{
 		for (i = n; i > 97; i--)
 		{
-			_putchar(i + '0');
+			if (i > 99)
+			{
+				_putchar((i / 100) + '0');
+				_putchar((i % 100) + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 			if (i > 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
