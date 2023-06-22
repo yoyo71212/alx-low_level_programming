9458f2c95c7a732a65ca027e47757174e542742b
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-22 15:17:31 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,21 +13,24 @@ void print_triangle(int size)
 		_putchar('\n');
 	}
 
-	int i;
-
-	for (i = 1; i < size + 1; i++)
+	else
 	{
-		int j;
+		int i;
 
-		for (j = 1; j <= size - i; j++)
+		for (i = 1; i < size + 1; i++)
 		{
-			_putchar(' ');
-		}
+			int j;
 
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
