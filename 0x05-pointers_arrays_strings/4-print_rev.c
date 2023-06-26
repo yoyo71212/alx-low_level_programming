104489a47c6d70f10af005d72c769295d745c946
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-25 21:05:41 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ *
+ * @s: The pointer to the first character in the string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	char *temp = s;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+
+	for (; temp > s; temp--)
+	{
+		_putchar(*temp);
+	}
+
+	_putchar('\n');
+}
