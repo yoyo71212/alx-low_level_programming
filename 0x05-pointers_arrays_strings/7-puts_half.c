a123ed83831aca9e4eb98852294c95eb419ffdf7
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-27 17:13:23 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,18 +12,15 @@ void puts_half(char *str)
 	int length, i, startI;
 
 	char *start = str;
-	
-	char *end = start + strlen(str);
-	
-	length = end - start;
 
-	if (length % 2 == 0)
+	char *end = start + strlen(str);
+
+	length = end - start;
+	startI = length / 2;
+
+	if (length % 2 != 0)
 	{
-		startI = length / 2;
-	}
-	else
-	{
-		startI = (length - 1) / 2;
+		startI += 1;
 	}
 
 	for (i = startI; i < length; i++)
