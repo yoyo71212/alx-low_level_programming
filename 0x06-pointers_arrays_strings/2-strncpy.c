bb829c7356252a9f10f2726f3b907c3b844fc338
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-29 16:28:01 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncopy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *temp = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	while (n > 0)
+	{
+		*dest = '\0';
+		n--;
+	}
+
+	return (temp);
+}
