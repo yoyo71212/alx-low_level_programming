23d0ed55af16138db418909973660d0d0b4d2f04
Yusuf Swelam <yusufswelam2@gmail.com>
2023-06-30 09:25:04 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using the ROT13 algorithm
+ *
+ * @s: The pointer to the string
+ *
+ * Return: The pointer to the string after encoding
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char *org = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char *rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (s[i] == org[j])
+			{
+				s[i] = rot[j];
+			}
+		}
+	}
+
+	return (s);
+}
