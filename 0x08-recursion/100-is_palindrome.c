ef12abc4960733ec363ebf12e6bc4f8d2b89d044
Yusuf Swelam <yusufswelam2@gmail.com>
2023-07-06 07:12:02 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * palindromeHelper - Checks if string is a palindrome
+ *
+ * @s: The string
+ * @start: The first/left most character to be checked
+ * @end: The last/right most character to be checked
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int palindromeHelper(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	else
+	{
+		return (palindromeHelper(s, start + 1, end - 1));
+	}
+}
+
+/**
+ * is_palindrome - Checks if string is a palindrome
+ *
+ * @s: The string
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+
+	if (c < 2)
+	{
+		return (1);
+	}
+
+	return (palindromeHelper(s, 0, c - 1));
+}
