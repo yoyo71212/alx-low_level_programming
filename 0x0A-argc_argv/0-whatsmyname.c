3dcfe209f619aa017495d6633b473868bd127ec1
Yusuf Swelam <yusufswelam2@gmail.com>
2023-07-06 22:00:32 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
