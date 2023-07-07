ad1e9f2b16ce2258f09d41fdd086bdf1b1a09f88
Yusuf Swelam <yusufswelam2@gmail.com>
2023-07-06 23:25:16 -0700

    2-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,10 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc >= 1)
-	{
-		printf("%s\n", argv[0]);
-	}
+	(void)argv;
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if (argc >= 1)
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[0]);
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
