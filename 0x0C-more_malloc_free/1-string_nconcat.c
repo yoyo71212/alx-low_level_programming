54aaa25019d18787e036cde6505dd8b56dee53c8
Yusuf Swelam <yusufswelam2@gmail.com>
2023-07-13 10:21:11 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = strlen(s2);
 	}
 
-	res = malloc(strlen(s1) + n + 1);
+	res = malloc(sizeof(char) * (strlen(s1) + n + 1));
 	
 	if (res == NULL)
 	{
