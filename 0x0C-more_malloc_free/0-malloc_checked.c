eb82f21aa6ebc9d1f87b2012b5f27d192374cf69
Yusuf Swelam <yusufswelam2@gmail.com>
2023-07-13 09:48:55 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,7 +6,8 @@
  * @b: The number of bytes to allocate
  *
  * Return: If malloc fails
- * the function should cause normal process termination with a status value of 98,
+ * the function should cause normal process termination
+ * with a status value of 98,
  * pointer to the allocated memory otherwise
  */
 void *malloc_checked(unsigned int b)
