3fe6d0e5695b34331ad8bc945e74f158d9333490
Yusuf Swelam <yusufswelam2@gmail.com>
2023-07-17 05:38:59 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog {
+/**
+ * struct dog - a dog
+ *
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: A dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
