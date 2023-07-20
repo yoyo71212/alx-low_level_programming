45fac0da9d9b52f4274dd786d1ef78b662783127
Yusuf Swelam <yusufswelam2@gmail.com>
2023-07-20 05:23:42 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < (int) n; i++)
 	{
-		if (i == (int) n - 1 || *separator == '\0')
+		if (i == (int) n - 1 || separator == NULL)
 		{
 			printf("%d", va_arg(args, int));
 		}
