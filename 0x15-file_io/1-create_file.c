7844190cb0d9607c91c225585ac7c329f789e0a2
Yusuf Swelam <yusufswelam2@gmail.com>
2023-08-21 09:41:51 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - Creates a file
+ *
+ * @filename: The name of the file to create
+ * @text_content: A NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	ssize_t bytes, length;
+
+	bytes = 0;
+	length = strlen(text_content);
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f < 0)
+	{
+		return (-1);
+	}
+	if (length)
+	{
+		bytes = write(f, text_content, length);
+	}
+	close(f);
+
+	if (bytes == length)
+	{
+		return (1);
+	}
+
+	return (-1);
+}
