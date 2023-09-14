a52102f5f3b7780ff9e9a9b6fec74350f7ab73fd
Yusuf Swelam <yusufswelam2@gmail.com>
2023-09-14 07:54:46 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
