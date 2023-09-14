46166ab8ddf2e40793ac0e2e5cbfc9a9c73b5437
Yusuf Swelam <yusufswelam2@gmail.com>
2023-09-14 07:56:16 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 /**
+ * print_dlistint - Prints all the elements of a dlistint_t list
+ *
+ * @h: Pointer to the head of the list
+ *
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
