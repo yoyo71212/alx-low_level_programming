58169739a6aa2274913a40ca2731626261716dee
Yusuf Swelam <yusufswelam2@gmail.com>
2023-10-07 09:11:30 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,13 +18,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
+	res->size = size;
 	res->array = malloc(size * sizeof(hash_node_t *));
 	if (!(res->array))
 	{
 		free(res);
 		return (NULL);
 	}
-	res->size = size;
 
 	for (i = 0; i < size; i++)
 	{
